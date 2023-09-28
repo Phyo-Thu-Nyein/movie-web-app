eb951230901bcafdb294b230b0f39b315777af1c
Phyo-Thu-Nyein <phyothunyein14@gmail.com>
2023-09-28 18:22:22 +0630

    if there're no recommeded movies, a txt is shown

--- src/app/recommendations/recommendations.component.ts
+++ src/app/recommendations/recommendations.component.ts
@@ -15,8 +15,11 @@ export class RecommendationsComponent implements OnInit {
   
   rcmdSub: Subscription = new Subscription();
 
+  // data!: RcmdMovies;
   rcmdMovieID!: number;
   rcmdMovieList: Result[] = [];
+  rcmd_movies!: number;
+  // NoResult: any;
 
   ngOnInit(): void {
     this.rcmdMovieID = this.activatedRoute.snapshot.params['movieid'];
@@ -28,6 +31,13 @@ export class RecommendationsComponent implements OnInit {
     this.rcmdSub = result.subscribe({
       next: (response: RcmdMovies) => {
         this.rcmdMovieList = response.results!;
+        console.log("total results right below")
+        console.log(response.total_results);
+        this.rcmd_movies = response.total_results!;
+        // this.total_pages = response.total_pages!;
+        // if (response.total_results == 0) {
+        //   this.NoResult = null;
+        // }
       }
     })
   }
